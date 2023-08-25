6e7c94e6fdde8db10c0c8bba3876534fffd7785a
e4ma-okafor <ifeomasandra13@gmail.com>
2023-08-25 03:56:55 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,13 @@ const App = () => {
   const colorsArr = Object.entries(colorList);
   //console.log(colorsArr);
   
-  const [bgColor, setbgColor] = useState(0); 
-  //const [displayName, setdisplayName] = useState();
-  //const [displayMode, setdisplayMode] = useState("name");
+  const [bgColor, setbgColor] = useState(0);   
   const [colorName, colorCode] = colorsArr[bgColor];  
 
   useEffect(() => {    
     document.body.style.backgroundColor = colorCode;
-    document.title = `Background Color Flipper - ${colorName}`;    
-    }      
+    document.title = `Background Color Flipper - ${colorName}`; 
+    }   
   );  
 
   const changeColor = () => {
@@ -28,15 +26,15 @@ const App = () => {
         <div className='nav-container'>
           <h2>Color Flipper</h2>
           <ul className='nav-links'>
-            <li><a href="#">Name</a></li>
-            <li><a href="#">Hex</a></li>            
+            <li><a href="#" className='color'>Name</a></li>
+            <li><a href="#" className='code'>Hex</a></li>            
           </ul>
         </div>
       </nav>
       <section>
         <div className='main-container'>
         <div className='colorFlipper'>
-          <h2 className='color-text'>Background Color: <span className='displayText'>{colorName}</span></h2>
+        <h2 className='color-text'>Background Color: {colorName ? colorName : colorCode}</h2>
           <button className='btn' onClick={changeColor}>
             Change Color
           </button>
