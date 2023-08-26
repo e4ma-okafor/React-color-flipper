bcbc43e3018cd18667d507bb457e933fe59ea045
e4ma-okafor <ifeomasandra13@gmail.com>
2023-08-26 14:49:06 +0100

    Updated mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,47 @@ import './App.css'
 import { useEffect, useState } from 'react'
 import { colorList } from './utils/data'
 
-const App = () => {
-  const colorsArr = Object.entries(colorList);
-  //console.log(colorsArr);
+function App() {
+  const colorsArr = Object.entries(colorList).map(([color, hex]) => ({
+    color,
+    hex,
+  }));
+  console.log(colorsArr);
   
-  const [bgColor, setbgColor] = useState(0);   
-  const [colorName, colorCode] = colorsArr[bgColor];      
+  const [bgColor, setbgColor] = useState(changeColor());  
+  const [displayMode, setdisplayMode] = useState('color');     
 
-  useEffect(() => {    
-    document.body.style.backgroundColor = colorCode;
-    document.title = `Background Color Flipper - ${colorName}`;          
-    }   
-  );   
+  useEffect(() => {     
+    document.body.style.backgroundColor = bgColor.hex;
+    document.title = `Background Color Flipper - ${bgColor.color}`;    
+    
+    const updateColorDisplay = () => {
+      if (displayMode === 'color') {
+        return bgColor.color;
+      } else if (displayMode === 'hex') {
+        return bgColor.hex.toUpperCase();
+      }
+    }
 
-  const changeColor = () => {
-    const randColorIndex = Math.floor(Math.random() * colorsArr.length);
-    setbgColor(randColorIndex);        
-  }; 
+    const colorDisplay = document.getElementById('display');
+    if (colorDisplay) {
+      colorDisplay.innerText = updateColorDisplay();
+    }
+    }, [bgColor, displayMode]   
+  );  
+  
+  function changeColor() {
+    const randomColorIndex = Math.floor(Math.random() * colorsArr.length);
+    return colorsArr[randomColorIndex];
+  }
+
+  const handleChangeColor = () => {
+    setbgColor(changeColor());
+  }
+
+  const handleDisplayMode = (mode) => {
+    setdisplayMode(mode);
+  }
  
   return (
     <>
@@ -26,16 +50,16 @@ const App = () => {
         <div className='nav-container'>
           <h2>Color Flipper</h2>
           <ul className='nav-links'>
-            <li><a href="#">Name</a></li>
-            <li><a href="#">Hex</a></li>            
+            <li><a href="#" onClick={() => handleDisplayMode('color')}>Name</a></li>
+            <li><a href="#" onClick={() => handleDisplayMode('hex')}>Hex</a></li>            
           </ul>
         </div>
       </nav>
       <section>
         <div className='main-container'>
         <div className='colorFlipper'>
-        <h2 className='color-text'>Background Color: <span className='display'>{colorName}</span></h2>
-          <button className='btn' onClick={changeColor}>
+        <h2 className='color-text'>Background Color: <span id='display'></span></h2>
+          <button className='btn' onClick={handleChangeColor}>
             Change Color
           </button>
         </div>
